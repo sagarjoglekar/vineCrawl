ed4cb7bb4dc77e884977b1d41ba888502c75a1bb
Sagar Joglekar <sagarjoglekar@gmail.com>
2015-11-25 15:23:43 +0000

    Get Popular Vine channels

--- vineCrawl.py
+++ vineCrawl.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python
-
 #Trying to use Vine API to get some videos
 
 import re
@@ -9,4 +7,31 @@ import time
 import random
 import json
 import sys
+import requests
 from multiprocessing import Pool
+
+
+class vineCrawler(Exception):
+	def __init__(self):
+		self.name = "Vine Crawler"
+		self.popular = None
+
+	def now_time(self):
+   		now=dt.datetime.now()
+   		return int(time.mktime(now.timetuple()))
+
+   	def getPopular(self):
+		self.popular = requests.get("https://api.vineapp.com/timelines/popular")
+		return self.popular
+
+
+if __name__ == '__main__':
+	print "initiating Vine Crawler from the popualr ones"
+	procPool = Pool(12)
+
+	crawler = vineCrawler();
+	popular = crawler.getPopular();
+	print popular
+
+
+
