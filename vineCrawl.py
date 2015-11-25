753b035ea47ab360e63499d74f2b07498bb03773
Sagar Joglekar <sagarjoglekar@gmail.com>
2015-11-25 14:04:11 +0000

    testing the waters

--- vineCrawl.py
+++ vineCrawl.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python
 
-"" Trying to use Vine API to get some videos ""
+#Trying to use Vine API to get some videos
+
+import re
+import datetime as dt
+from datetime import datetime
+import time
+import random
+import json
+import sys
+from multiprocessing import Pool
