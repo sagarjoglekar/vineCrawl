c1803a90610b8b48718e811810dc8ca906e075ff
Sagar Joglekar <sagarjoglekar@gmail.com>
2015-11-27 18:24:20 +0000

    remove subprocessing because the main process dies for some reason because of that ..

--- vineCrawl.py
+++ vineCrawl.py
@@ -15,16 +15,13 @@ if __name__ == '__main__':
 	print "initiating Vine Crawler from the popualr ones"
 	procPool = Pool(1)
 	procRegister = []
-
+	crawler = vineCrawler(procPool)
+	parser = parsePopular(procPool)
 	while True:
 		timeStamp = now_time()
 		os.makedirs(str(timeStamp))
-
-		crawler = vineCrawler(procPool, str(timeStamp))
-		popular = crawler.getPopular()		
-
-		parser = parsePopular(popular , procPool,str(timeStamp))
-		procRegister = parser.decomposePopular()
+		popular = crawler.getPopular(str(timeStamp))		
+		procRegister = parser.decomposePopular(popular, str(timeStamp))
 		time.sleep(120)
 		for i in range(0 , len(procRegister)):
 			Popen.kill(procRegister[i])
