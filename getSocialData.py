545554b270cbf388c5876fdff556da98a353c405
Sagar Joglekar <sagarjoglekar@gmail.com>
2015-12-01 15:15:16 +0000

    Collect user timeline and post data which contains comments

--- getSocialData.py
+++ getSocialData.py
@@ -1,3 +1,4 @@
+
 #Class that starts everything
 
 from multiprocessing import Pool
@@ -12,22 +13,101 @@ import sys
 import requests
 import os
 import wget
+import pickle
+
+visitedList = "visited.data"
+root ="Data/"
+timeline = "userTimeline"
+postData = "postData"
 
 def now_time():
-   		now=dt.datetime.now()
-   		return int(time.mktime(now.timetuple()))
+	now=dt.datetime.now()
+	return int(time.mktime(now.timetuple()))
+
+def getVisited():
+	visited = []
+	try:
+		f = open(visitedList, 'rb')
+		visited = pickle.load(f)
+	except IOError:
+		f = open(visitedList,"a+")
+		pickle.dump([],f)
+	return visited
+
+
+def updateVisited(visited):
+	with open(visitedList, 'wb') as f:
+		pickle.dump(visited, f)
+
 
 def walkLevel1Dir(root):
 	count = 0
+	dirList = []
+	filesList = []
 	for path, dirs, files in os.walk(root):
 		if count > 0:
-			return dirs , files
-		for dir in dirs:
-			print "Directory : " + dir
+			return dirList , fileList
+		dirList = dirs
+		fileList = files
 		count = count + 1
 
-if __name__ == '__main__':
 
-	dirs, files = walkLevel1Dir("Data/");
-	print len(files)
-	print len(dirs)
+def getPopularFile(rootDir):
+		f = open(rootDir + '/popular.json' ,'r')
+		data = json.load(f)
+		return data
+
+def getSocialData(popular , timeline , posts):
+		records = popular['data']['records']
+		for i in range (0 , len(records)):
+			print "Getting Post data..."
+			#get Post data
+			postID = records[i]['postId']
+			postData = requests.get("https://api.vineapp.com/timelines/posts/"+str(postID))
+			post = open( posts + "/" + str(postID) + '.json', 'w')
+			json.dump(postData.json(), post)
+
+			#get USer Timeline
+			print "Getting User data..."
+			userId = records[i]['userId']
+			userData = requests.get("https://api.vineapp.com/timelines/users/"+str(userId)+"?size=50");
+			user = open( timeline + "/" + str(userId) + '.json', 'w')
+			json.dump(userData.json(), user)
+
+
+
+
+
+if __name__ == '__main__':
+	dirs,files = walkLevel1Dir(root)
+	visited = getVisited()
+	
+	for dir in dirs:
+		if dir not in visited:
+			timeline = root + dir + "/" + timeline
+			posts = root + dir + "/" + postData
+
+			if not os.path.exists(posts):
+				os.makedirs(posts)
+			if not os.path.exists(timeline):
+				os.makedirs(timeline)
+			
+			dataRoot = root + dir
+			popular = getPopularFile(dataRoot)
+
+			getSocialData(popular , timeline , posts)
+
+			visited.append(dir)
+			updateVisited(visited)
+			print dir
+			break
+
+
+			
+			
+
+
+
+
+
+
