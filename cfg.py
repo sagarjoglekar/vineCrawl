e950ca39bfa9cb96b397252b407281caa6f9afb1
NetSys <sagarjoglekar@nmsdps000222.nms.kcl.ac.uk>
2016-02-12 17:10:30 +0000

    remove files from wrong dest

--- cfg.py
+++
@@ -1,8 +0,0 @@
-
-log_dir = '/home/k1206443/data/about.me_oxford/facebook/log/'
-
-vision_start_num = 0
-vision_process_num = 50 
-vision_data_file = '/home/k1206443/cz-exp/about.me/data-org/data/facebook010203-avatar-lists.csv.gz'
-vision_save_dir = '/home/k1206443/data/about.me_oxford/facebook/'
-
